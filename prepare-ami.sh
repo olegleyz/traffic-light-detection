7b145934ce020cb8d0fb7012550daa8858f7d4c8
Oleg Leyzerov <olegleyz@gmail.com>
2017-09-28 18:38:35 +0200

    fixing tfrecords generation

--- prepare-ami.sh
+++ prepare-ami.sh
@@ -36,15 +36,15 @@ rm ssd_mobilenet_v1_coco_11_06_2017.tar.gz
 wget http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_11_06_2017.tar.gz
 tar -xf faster_rcnn_resnet101_coco_11_06_2017.tar.gz
 rm faster_rcnn_resnet101_coco_11_06_2017.tar.gz
-echo "generating tfrecords"
-chmod +x "bosch-to-tfrecords.py"
-./bosch-to-tfrecords.py
-chmod +x "bosch-to-tfrecords-test.py"
-./bosch-to-tfrecords-test.py
 cd ..
 echo "installing dependencies"
 pip install tensorflow-gpu
 pip install tqdm
+echo "generating tfrecords"
+chmod +x "bosch-to-tfrecords.py"
+./bosch-to-tfrecords.py --output_path=data/train.record
+chmod +x "bosch-to-tfrecords-test.py" 
+./bosch-to-tfrecords-test.py --output_path=data/test.record
 
 # set up protobuf
 echo "set up protobuf"
